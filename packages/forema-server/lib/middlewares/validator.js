12b9332eb16869a5caa2c5ebaf6aedfdba4ea00f
fmartin <fedemartin94@gmail.com>
2020-05-08 05:52:54 -0300

    fixed validator to compile schema refs

--- packages/forema-server/lib/middlewares/validator.js
+++ packages/forema-server/lib/middlewares/validator.js
@@ -1,13 +1,19 @@
 import Ajv from 'ajv';
+import discussionSchema from 'shared/schemas/Discussion.json';
+import forumSechema from 'shared/schemas/Forum.json';
+import opinionSchema from 'shared/schemas/opinion.json';
+import userSchema from 'shared/schemas/user.json';
 
 export default () => {
-  console.log('Configurando validator...');
-  const validator = new Ajv();
+  // load and compile schemas
+  var validator = new Ajv({
+    schemas: [userSchema, forumSechema, discussionSchema, opinionSchema],
+  });
 
-  //middleware
+  // middleware
   return async (ctx, next) => {
     ctx.validate = (schema, data) => {
-      if (!validator.validate(schema, data)) {
+      if (!validator.validate({ $schema: schema, ...data })) {
         ctx.throw(400, validator.errorsText());
       }
     };
