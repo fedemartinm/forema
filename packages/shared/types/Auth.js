83948f1adfa9f9ae53b93a4b1e189ffc9217f230
fmartin <fedemartin94@gmail.com>
2020-05-15 23:40:24 -0300

    fix

--- packages/shared/types/Auth.js
+++ packages/shared/types/Auth.js
@@ -21,5 +21,5 @@ export interface IAuth {
    * Get current authenticated user.
    * @returns User object or undefined if not exists.
    */
-  getCurrentUser(): Promise<User>;
+  getCurrentUser(): Promise<void | User>;
 }
